dbf16c44574e7e4871f8e98148b092eaaee6bf32
R Balloray <balloray@gmail.com>
2020-09-12 16:20:53 -0500

    deploy

--- JenkinsDeploy.groovy
+++ JenkinsDeploy.groovy
@@ -1,3 +1,8 @@
+
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+
+
+
 properties([
     [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
     parameters([
@@ -5,15 +10,58 @@ properties([
         booleanParam(defaultValue: false, description: 'Please select to destroy all changes to the environment', name: 'destroyChanges'), 
         string(defaultValue: '', description: 'Please provide the docker to deploy ', name: 'selectedDockerImage', trim: true), 
         choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment')
-        ])
-        ])
+    ])
+])
 
-        
-println(
+
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers: 
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
     """
-    Apply changes: ${params.applyChanges}
-    Destroy changes: ${params.destroyChanges}
-    Docker  image:  ${params.selectedDockerImage}
-    Environment: ${params.environment}
-    """
-)
+podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+    node(k8slabel){
+        stage("Checkout SCM"){
+          git 'https://github.com/balloray/jenkins-class.git'
+        }
+
+
+
+
+        stage("Apply/Plan") {
+            container("fuchicorptools") {
+                sh 'kubectl --version'
+            }
+        }
+    }
+}
+
